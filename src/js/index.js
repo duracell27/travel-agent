a4f797d8be2b1dfd2801a86d5bf9ca108083d399
Duracell27 <volodymyrshmidt@gmail.com>
2024-03-17 23:19:14 +0200

    response start

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
-// import mobileNav from './modules/mobile-nav.js';
-// mobileNav();
+import mobileNav from './modules/mobile-nav.js';
+mobileNav();
 import { easepick } from "@easepick/bundle";
 import autoComplete from "@tarekraafat/autocomplete.js";
 import Swiper from "swiper/bundle";
